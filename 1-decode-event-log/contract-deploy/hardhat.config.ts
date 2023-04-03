7c82de88979e786cdef56912a8a110240aa89673
syjn99 <syjn99@gmail.com>
2023-04-03 23:18:46 +0900

    Add Deploy code and setup for env

--- 1-decode-event-log/contract-deploy/hardhat.config.ts
+++ 1-decode-event-log/contract-deploy/hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
 };
 
 export default config;
--- 1-decode-event-log/contract-deploy/scripts/deploy.ts
+++ 1-decode-event-log/contract-deploy/scripts/deploy.ts
@@ -1,23 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Account balance: ${(await deployer.getBalance()).toString()}`);
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const PoolCreate = await ethers.getContractFactory("PoolCreate");
+  const poolCreate = await PoolCreate.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`PoolCreate deployed to ${poolCreate.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
